fd4b29e34ccfec6445434ba377ab3de041389eb7
SavaStefanCristian <savastefancristian@gmail.com>
2024-11-01 20:13:11 +0200

    Am creat clasa abstracta Object

---
+++ DeLaGaze/Object.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+class Object {
+public:
+	virtual void render() = 0;
+	//OR
+	//virtual void render(int x, int y) = 0;
+	virtual void print() = 0;
+private:
+};
