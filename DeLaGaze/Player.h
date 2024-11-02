383570a1fd171675f6ea33e64fd2a0daa66a123c
Alex <flalex216@gmail.com>
2024-11-02 19:25:46 +0200

    Am adaugat atribute clasei Player

--- DeLaGaze/Player.h
+++ DeLaGaze/Player.h
@@ -3,9 +3,24 @@
 #include <cstdint>
 class Player: public Object{
 private:
-	uint8_t hp : 2;
-	uint8_t score;
-	int points;
-	int x, y;
+	uint8_t m_hp : 2;
+	uint8_t m_score;
+	int m_points;
+	int m_x, m_y;
+public:
+	Player();
+	~Player();
+	void render() override;
+	void print() override;
+	void SetX(int x);
+	void SetY(int y);
+	int GetX();
+	int GetY();
+	void setHp(uint8_t hp);
+	void SetScore(uint8_t score);
+	void SetPoints(int points);
+	uint8_t GetHp();
+	uint8_t GetScore();
+	int GetPoints();
 };
 
