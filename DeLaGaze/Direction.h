310c4c661d2703c38be3c8f28e824e3f5a69fcc4
stefan_d799 <dinustefan1996@yahoo.com>
2024-11-09 15:20:37 +0000

    Am creat doua clase enum, pentru facing, si starea jucatorului.

---
+++ DeLaGaze/Direction.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+enum class Direction : uint8_t {
+	North,
+	East,
+	South,
+	Werst
+};
---
+++ DeLaGaze/State.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+enum class State : uint8_t {
+	Idle,
+	Moving,
+	Shooting,
+	MovingShooting
+};
