7f3a25b7db7a59f876a21e9bf87f723a87eec364
stefan_d799 <dinustefan1996@yahoo.com>
2024-11-10 20:10:18 +0000

    Am adaugat pozitie pentru clasa object si am creat un constructor

--- DeLaGaze/Object.cpp
+++ DeLaGaze/Object.cpp
@@ -1,1 +1,3 @@
 #include "Object.h"
+
+Object::Object(const std::pair<int, int>& pos) : m_pos{ pos } {};
--- DeLaGaze/Object.h
+++ DeLaGaze/Object.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <utility>
 
 class Object {
 public:
+	 Object(const std::pair<int,int>& pos);
+
 	virtual void render() = 0;
 	//OR
 	//virtual void render(int x, int y) = 0;
 	virtual void print() = 0;
 	virtual bool canMoveHere(int i,int j) = 0;
 private:
+	std::pair<int, int> m_pos;
 };
