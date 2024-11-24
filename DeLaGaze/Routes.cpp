cd09a614f440c4d8d76962d7eeee346d821c9930
Alex <flalex216@gmail.com>
2024-11-24 23:03:24 +0200

    Am adaugat ruta pentru a adauga un player in BD

--- DeLaGaze/Routes.cpp
+++ DeLaGaze/Routes.cpp
@@ -6,7 +6,6 @@ Routes::Routes(crow::SimpleApp& app)
         try {
             std::vector<crow::json::wvalue> players_json;
 
-            // Iterate through all players and add to JSON response
             for (const auto& player : player_storage.GetAllPlayers()) {
                 players_json.push_back(crow::json::wvalue{
                     {"username", player.GetUsername()},
@@ -16,7 +15,6 @@ Routes::Routes(crow::SimpleApp& app)
                     {"upgrade_bs", player.GetBulletSpeedUpgrade()}
                     });
             }
-
             return crow::json::wvalue{ players_json };
         }
         catch (const std::exception& e) {
@@ -26,4 +24,29 @@ Routes::Routes(crow::SimpleApp& app)
             };
         }
     });
+    CROW_ROUTE(app, "/players/add")([&](const crow::request& req) {
+        try {
+            crow::json::rvalue body = crow::json::load(req.body);
+            if (!body) {
+                return crow::response(400, "Invalid JSON body");
+            }
+
+            Player new_player;
+            new_player.SetUsername(body["username"].s());
+            new_player.SetScore(body["score"].i());
+            new_player.SetPoints(body["points"].i());
+            new_player.SetBulletSpeed(body["firerate"].i());
+            new_player.SetBulletSpeedUpgrade(body["upgrade_bs"].i());
+
+            if (player_storage.AddPlayer(new_player)) {
+                return crow::response(201, "Player added successfully");
+            }
+            else {
+                return crow::response(500, "Error adding player");
+            }
+        }
+        catch (const std::exception& e) {
+            return crow::response(500, "Internal server error: " + std::string(e.what()));
+        }
+        });
 }
