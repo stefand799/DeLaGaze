12011e7ee07bfca8375f1cbcdb8577ee60811baf
StefannnJ <stefmimlaiml@gmail.com>
2024-11-24 20:37:15 +0200

    am dat commit fara sa salvez toate fisierel ups

--- DeLaGaze Client/LoginScreen.cpp
+++ DeLaGaze Client/LoginScreen.cpp
@@ -3,19 +3,7 @@
 LoginScreen::LoginScreen(QWidget *parent)
 	: QWidget(parent)
 {
-	//ui.setupUi(this);
-
-    usernameInput = new QLineEdit(this);
-    usernameInput->setGeometry(20, 60, 200, 30);
-    usernameInput->setPlaceholderText("Username");
-
-   
-    loginButton = new QPushButton("Login", this);
-    loginButton->setGeometry(20, 140, 90, 30);
-
-
-    //connect(loginButton, &QPushButton::clicked, this, &LoginScreen::loginClicked);
-    //connect(registerButton, &QPushButton::clicked, this, &LoginScreen::registerClicked);
+	ui.setupUi(this);
 }
 
 LoginScreen::~LoginScreen()
--- DeLaGaze Client/MainScreen.h
+++ DeLaGaze Client/MainScreen.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "qwidget.h"
+#include "qpushbutton.h"
+
 class MainScreen:public QWidget{
 	Q_OBJECT
 public:
 	MainScreen(QWidget* parent = nullptr);
 private:
-	
+	QPushButton* logoutButton;
+	QPushButton* upgradesButton;
+	QPushButton* playButton;
 };
 
