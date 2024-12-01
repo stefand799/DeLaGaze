3c6e6378c03972d2ea1e5c82e2455166151317c4
Alex <flalex216@gmail.com>
2024-12-01 23:00:37 +0200

    Am implementat o parte din rute. De vazut ce facem in cazul in care un player introduce un nume deja existent in baza de date

--- DeLaGaze/Routes.cpp
+++ DeLaGaze/Routes.cpp
@@ -1,12 +1,12 @@
 #include "Routes.h"
 
-Routes::Routes(crow::SimpleApp& app)
+void Routes::Run(database::PlayerStorage& playerStorage)
 {
-    CROW_ROUTE(app, "/players")([&]() {
+    CROW_ROUTE(m_app,"/players")([&]() {
         try {
             std::vector<crow::json::wvalue> players_json;
 
-            for (const auto& player : m_playerStorage.GetAllPlayers()) {
+            for (const auto& player : playerStorage.GetAllPlayers()) {
                 players_json.push_back(crow::json::wvalue{
                     {"username", player.GetUsername()},
                     {"score", player.GetScore()},
@@ -24,7 +24,7 @@ Routes::Routes(crow::SimpleApp& app)
             };
         }
     });
-    CROW_ROUTE(app, "/players/add")([&](const crow::request& req) {
+    CROW_ROUTE(m_app, "/players/add")([&](const crow::request& req) {
         try {
             crow::json::rvalue body = crow::json::load(req.body);
             if (!body) {
@@ -38,7 +38,7 @@ Routes::Routes(crow::SimpleApp& app)
             new_player.SetBulletSpeed(body["firerate"].i());
             new_player.SetBulletSpeedUpgrade(body["upgrade_bs"].i());
 
-            if (player_storage.AddPlayer(new_player)) {
+            if (playerStorage.AddPlayer(new_player)) {
                 return crow::response(201, "Player added successfully");
             }
             else {
@@ -49,4 +49,46 @@ Routes::Routes(crow::SimpleApp& app)
             return crow::response(500, "Internal server error: " + std::string(e.what()));
         }
         });
+}
+crow::response Routes::AddPlayerToDatabase(database::PlayerStorage& playerStorage, const crow::request& req) {
+    crow::json::rvalue body = crow::json::load(req.body);
+    if (!body) {
+        return crow::response(400, "Invalid JSON body");
+    }
+	//de stabilit ce facem in caz ca avem deja un player cu acelasi username
+	if (playerStorage.GetPlayer(body["username"].s())) {
+		return crow::response(400, "Player already exists");
+	}
+    Player p;
+	p.SetUsername(body["username"].s());
+	p.SetScore(0);
+	p.SetPoints(0);
+	p.SetBulletSpeed(1);
+	p.SetBulletSpeedUpgrade(false);
+	if (playerStorage.AddPlayer(p)) {
+		return crow::response(201, "Player added successfully");
+	}
+	else {
+		return crow::response(500, "Error adding player");
+	}
+}
+crow::response Routes::UpdatePlayerFirerate(database::PlayerStorage& playerStorage, const crow::request& req,Player& player, int x)
+{
+	player.SetBulletSpeed(x);
+	if (playerStorage.UpdatePlayer(player)) {
+		return crow::response(200, "Player updated successfully");
+	}
+	else {
+		return crow::response(500, "Error updating player");
+	}
+}
+crow::response Routes::UpdatePlayerBulletSpeed(database::PlayerStorage& playerStorage,Player& player, const crow::request& req)
+{
+    player.SetBulletSpeedUpgrade(true);
+	if (playerStorage.UpdatePlayer(player)) {
+		return crow::response(200, "Player updated successfully");
+	}
+	else {
+		return crow::response(500, "Error updating player");
+	}
 }
--- DeLaGaze/Routes.h
+++ DeLaGaze/Routes.h
@@ -4,19 +4,17 @@
 class Routes
 {
 	public:
-		Routes(crow::SimpleApp& app);
 		void Run(database::PlayerStorage& playerStorage);
 	private:
-		crow::response AddPlayerToDatabase(database::PlayerStorage& playerStorage, const crow::request& req, const Player& Player);
-		crow::response UpdatePlayerFirerate(database::PlayerStorage& playerStorage, const crow::request& req, Player& Player);
-		crow::response UpdatePlayerBulletSpeed(database::PlayerStorage& playerStorage, const crow::request& req, Player& Player);
+		crow::response AddPlayerToDatabase(database::PlayerStorage& playerStorage, const crow::request& req);
+		crow::response UpdatePlayerFirerate(database::PlayerStorage& playerStorage, const crow::request& req,Player& player, int x);
+		crow::response UpdatePlayerBulletSpeed(database::PlayerStorage& playerStorage, Player& player, const crow::request& req);
 		crow::response GetPlayersFromDatabase(database::PlayerStorage& playerStorage);
 		crow::response FinishGame(database::PlayerStorage& playerStorage, const crow::request& req, std::vector<Player>& Players);
 		crow::response JoinGame(database::PlayerStorage& playerStorage, const crow::request& req, Player& Players);
 
 	private:
 		crow::SimpleApp m_app;
-		database::PlayerStorage m_playerStorage;
 
 };
 
