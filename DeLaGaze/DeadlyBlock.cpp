a5ffb97c549ee40f3ab9b88e5c2f2a852b8144e1
Alex <flalex216@gmail.com>
2024-12-08 13:29:36 +0200

    Am implementat functiile pentru clasa DeadlyBlock

---
+++ DeLaGaze/DeadlyBlock.cpp
@@ -0,0 +1,28 @@
+#include "DeadlyBlock.h"
+
+DeadlyBlock::DeadlyBlock(std::pair<size_t, size_t> pos) : Block{ pos }
+{
+}
+
+void DeadlyBlock::Render()
+{
+}
+
+void DeadlyBlock::Print() const
+{
+	std::cout << "\033[37;46m" << " " << "\033[0m"; //B
+}
+
+crow::json::wvalue DeadlyBlock::toJson()
+{
+    crow::json::wvalue jsonObj;
+	jsonObj["type"] = "DeadlyBlock";
+	jsonObj["x"] = m_pos.first;
+	jsonObj["y"] = m_pos.second;
+	return jsonObj;
+}
+
+bool DeadlyBlock::CanMoveHere(int i, int j)
+{
+    return true;
+}
